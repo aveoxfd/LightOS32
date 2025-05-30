cc26c9a574620769bac5ec78d7bc1966f8e0baba
.ffd <108579984+aveoxfd@users.noreply.github.com>
2025-05-30 08:42:07 +0300

    Update pic.h

--- LightOS/kernel/drivers/C/headers/pic.h
+++ LightOS/kernel/drivers/C/headers/pic.h
@@ -5,8 +5,8 @@
 //ports for PIC 8259
 #define PIC1_COMMAND 0x20   //controller PIC1 shadow command reg  
 #define PIC1_DATA    0x21   //controller PIC1 IMR Mask reg
-#define PIC2_COMMAND 0xA0   //
-#define PIC2_DATA    0xA1   //
+#define PIC2_COMMAND 0xA0   //controller PIC2 shadow command reg  
+#define PIC2_DATA    0xA1   //controller PIC2 IMR Mask reg
 #define PIC_EOI      0x20   // End-of-interrupt command code
 
 void pic_remap(const int, const int);
